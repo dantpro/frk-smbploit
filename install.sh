d9c27a2429a2f2d1465b44ec5585c57753361645
Miguel R <d4t4s3c@protonmail.com>
2020-03-14 12:53:11 +0100

    install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 #Author: d4t4s3c
 #Email:  d4t4s3c@protonmail.com
 #GitHub: www.github.com/d4t4s3c
@@ -10,51 +9,64 @@ r="\033[1;31m"
 v="\033[1;32m"
 a="\033[1;33m"
 nc="\e[0m"
+
 clear
 sleep 1
 
-#banner
+function banner(){
+       echo -e "$b ┌══════════════════════════════════════════┐"
+       echo -e "$b ║$v  ____  __  __ ____        _       _ _    $b║"
+       echo -e "$b ║$v / ___||  \/  | __ ) _ __ | | ___ (_) |_  $b║"
+       echo -e "$b ║$v \___ \| |\/| |  _ \| '_ \| |/ _ \| | __| $b║"
+       echo -e "$b ║$v  ___) | |  | | |_) | |_) | | (_) | | |_  $b║"
+       echo -e "$b ║$v |____/|_|  |_|____/| .__/|_|\___/|_|\__| $b║"   
+       echo -e "$b ║$v                    |_|                   $b║"
+       echo -e "$b ║$r  Author  $b: $a d4t4s3c                      $b║"
+       echo -e "$b ║$r  Email   $b: $a d4t4s3c@protonmail.com       $b║"
+       echo -e "$b ║$r  GitHub  $b: $a www.github.com/d4t4s3c       $b║"
+       echo -e "$b └══════════════════════════════════════════┘$b"
+}
+
+banner
+sleep 2
+
 echo ""
-echo -e "$b ============================================"
-echo -e "$b |$v  ____  __  __ ____        _       _ _    $b|"
-echo -e "$b |$v / ___||  \/  | __ ) _ __ | | ___ (_) |_  $b|"
-echo -e "$b |$v \___ \| |\/| |  _ \| '_ \| |/ _ \| | __| $b|"
-echo -e "$b |$v  ___) | |  | | |_) | |_) | | (_) | | |_  $b|"
-echo -e "$b |$v |____/|_|  |_|____/| .__/|_|\___/|_|\__| $b|"   
-echo -e "$b |$v                    |_|                   $b|"
-echo -e "$b |$r  Author  $b: $a d4t4s3c                      $b|"
-echo -e "$b |$r  Email   $b: $a d4t4s3c@protonmail.com       $b|"
-echo -e "$b |$r  GitHub  $b: $a www.github.com/d4t4s3c       $b|"
-echo -e "$b ============================================ $b"
+echo -e "$b[$v+$b]$a Installing Xterm $nc"
+sleep 4
+echo ""
+apt-get install -y xterm
 echo ""
 echo -e "$b[$v+$b]$a Add exploit to Metasploit-Framework $nc"
-sleep 2
-echo ""
+sleep 4
 cp SMBploit.rb /usr/share/metasploit-framework/modules/exploits/windows/smb
 echo ""
+echo -e "$b[$v+$b]$a Reloading Modules $nc"
+sleep 4
+xterm -hold -e "msfconsole -q -r reload.rc" &
+echo ""
 echo -e "$b[$v+$b]$a Installing Wine $nc"
-sleep 2
+sleep 4
 echo ""
 apt-get install -y wine
 echo ""
 echo -e "$b[$v+$b]$a Installing Wine32 $nc"
-sleep 2
+sleep 4
 echo ""
 dpkg --add-architecture i386 && apt-get install -y wine32
 echo ""
-sleep 2
-echo ""
 echo -e "$b[$v+$b]$a Update $nc"
-sleep 2
+sleep 4
 echo ""
 apt-get update -y && apt-get upgrade -y && apt-get dist-upgrade -y
 echo ""
 echo -e "$b[$v+$b]$a Testing Wine$nc"
 echo ""
-sleep 2
-echo -e "$b[$v+$b]$r Close the terminal when a$v cmd$r appers$b -$r Cierre la terminal cuando muestre un$v cmd$nc"
+sleep 4
+echo -e "$b[$v+$b]$r Close the popup when you present a$v cmd$nc $b-$r Cierre la ventana emergente cuando presente un$v cmd$nc"
 echo ""
-sleep 2
-wine cmd.exe
-echo ""
+sleep 4
+xterm -hold -e "wine cmd.exe" &
+echo -e "$b[$v+$b]$a Installation Completed $nc"
+echo""
+sleep 4
 exit
